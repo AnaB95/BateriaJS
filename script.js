413b02329d36332265f410cd84777a2c9383a129
Ana Belén <ana.b.95@hotmail.com>
2023-03-24 13:44:59 +0100

    primer sonido

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-
 "use strict";
-console.log ("Hola Mundo");
+
+let button1 = document.getElementById("button1");
+let crash = document.getElementById("crash");
+
+button1.addEventListener("click", () => {
+  crash.play();
+});
