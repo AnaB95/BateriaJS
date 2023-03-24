f0680d7990c3fa6babafaa0c629b6dfa4385e094
Monica <moirivilla@gmail.com>
2023-03-24 16:30:32 +0100

    Botones terminados

--- script.js
+++ script.js
@@ -20,3 +20,47 @@ let open = document.getElementById("open");
 button3.addEventListener("click", () => {
   open.play();
 });
+
+
+let button4 = document.getElementById("button4");
+let kick = document.getElementById("kick");
+
+button4.addEventListener("click", () => {
+  kick.play();
+});
+
+
+let button5 = document.getElementById("button5");
+let ride = document.getElementById("ride");
+
+button5.addEventListener("click", () => {
+  ride.play();
+});
+
+let button6 = document.getElementById("button6");
+let snare = document.getElementById("snare");
+
+button6.addEventListener("click", () => {
+  snare.play();
+});
+
+let button7 = document.getElementById("button7");
+let high = document.getElementById("high");
+
+button7.addEventListener("click", () => {
+  high.play();
+});
+
+let button8 = document.getElementById("button8");
+let low = document.getElementById("low");
+
+button8.addEventListener("click", () => {
+  low.play();
+});
+
+let button9 = document.getElementById("button9");
+let mid = document.getElementById("mid");
+
+button9.addEventListener("click", () => {
+  mid.play();
+});
